123c6b50888842b4b3785115c4860329501d734e
Dariusz Rybicki <darrarski@gmail.com>
2016-10-28 22:39:53 +0200

    Refactor ObservableArrayChangeEvent
    Replace extension with static equality function with just static
    equality function for the sake of readability. Also, it's not possible
    to create such extension for checking equality of two arrays that
    contains ObservableArrayChangeEvent objects. It is possible using
    plain equality function (not embedded as a static extension function),
    so to keep it consistent, will use plain equality functions.

--- DRRxObservableArray/ObservableArrayChangeEvent.swift
+++ DRRxObservableArray/ObservableArrayChangeEvent.swift
@@ -4,26 +4,22 @@ enum ObservableArrayChangeEvent<T> {
     case updated(indices: [Int], oldElements: [T], newElements: [T])
 }
 
-extension ObservableArrayChangeEvent where T: Equatable {
+func ==<T: Equatable>(lhs: ObservableArrayChangeEvent<T>, rhs: ObservableArrayChangeEvent<T>) -> Bool {
+    switch (lhs, rhs) {
+    case (.inserted(let indicesLeft, let elementsLeft),
+          .inserted(let indicesRight, let elementsRight)),
+         (.deleted(let indicesLeft, let elementsLeft),
+          .deleted(let indicesRight, let elementsRight)):
+        return indicesLeft == indicesRight &&
+            elementsLeft == elementsRight
 
-    static func ==(lhs: ObservableArrayChangeEvent<T>, rhs: ObservableArrayChangeEvent<T>) -> Bool {
-        switch (lhs, rhs) {
-        case (.inserted(let indicesLeft, let elementsLeft),
-              .inserted(let indicesRight, let elementsRight)),
-             (.deleted(let indicesLeft, let elementsLeft),
-              .deleted(let indicesRight, let elementsRight)):
-            return indicesLeft == indicesRight &&
-                   elementsLeft == elementsRight
+    case (.updated(let indicesLeft, let oldElementsLeft, let newElementsLeft),
+          .updated(let indicesRight, let oldElementsRight, let newElementsRight)):
+        return indicesLeft == indicesRight &&
+            oldElementsLeft == oldElementsRight &&
+            newElementsLeft == newElementsRight
 
-        case (.updated(let indicesLeft, let oldElementsLeft, let newElementsLeft),
-              .updated(let indicesRight, let oldElementsRight, let newElementsRight)):
-            return indicesLeft == indicesRight &&
-                   oldElementsLeft == oldElementsRight &&
-                   newElementsLeft == newElementsRight
-
-        default:
-            return false
-        }
+    default:
+        return false
     }
-
 }
