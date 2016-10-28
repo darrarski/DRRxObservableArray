2bc4db472e9bd184a96b164462b63a700617e028
Dariusz Rybicki <darrarski@gmail.com>
2016-10-28 20:14:47 +0200

    Add Equatable extension to ObservableArrayChangeEvent

--- DRRxObservableArray/ObservableArrayChangeEvent.swift
+++ DRRxObservableArray/ObservableArrayChangeEvent.swift
@@ -3,3 +3,11 @@ enum ObservableArrayChangeEvent<T> {
     case deleted(indices: [Int], elements: [T])
     case updated(indices: [Int], oldElements: [T], newElements: [T])
 }
+
+extension ObservableArrayChangeEvent where T: Equatable {
+
+    static func ==(lhs: ObservableArrayChangeEvent<T>, rhs: ObservableArrayChangeEvent<T>) -> Bool {
+        return false
+    }
+
+}
