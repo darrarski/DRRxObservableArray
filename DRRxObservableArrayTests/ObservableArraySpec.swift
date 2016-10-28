981bd806bb2022a8444cbfa33e282ec22a581afb
Dariusz Rybicki <darrarski@gmail.com>
2016-10-28 03:42:03 +0200

    Replace XCTestCase with QuickSpec

---
+++ DRRxObservableArrayTests/ObservableArraySpec.swift
@@ -0,0 +1,26 @@
+import Quick
+import Nimble
+
+class ObservableArraySpec: QuickSpec {
+
+    override func spec() {
+        describe("ObservableArray of Strings") {
+            var sut: ObservableArray<String>!
+
+            context("when initialized with array") {
+                beforeEach {
+                    sut = ObservableArray(["a", "b", "c"])
+                }
+
+                afterEach {
+                    sut = nil
+                }
+
+                it("should have correct elements") {
+                    expect(sut.elements).to(equal(["a", "b", "c"]))
+                }
+            }
+        }
+    }
+
+}
