942dda6414de68b512a3098ac7b8bc6912431b50
Dariusz Rybicki <darrarski@gmail.com>
2016-10-30 16:48:51 +0100

    Add documentation comments

--- DRRxObservableArray/ObservableArray.swift
+++ DRRxObservableArray/ObservableArray.swift
@@ -1,24 +1,36 @@
 import RxSwift
 
+/// ObservableArray of generic elements
 public struct ObservableArray<T> {
 
+    /// Contained elements
     public private(set) var elements: [T]
 
+    /// Initialize ObservableArray with given elements
+    ///
+    /// - Parameter elements: elements
     public init(_ elements: [T] = []) {
         self.elements = elements
     }
 
+    /// Count of elements in ObservableArray
     public var count: Int {
         return elements.count
     }
 
     // MARK: Mutating
 
+    /// Append element to ObservableArray
+    ///
+    /// - Parameter element: element
     public mutating func append(_ element: T) {
         elements.append(element)
         eventsSubject.onNext(.inserted(indices: [elements.count - 1], elements: [element]))
     }
 
+    /// Append multiple elements to ObservableArray
+    ///
+    /// - Parameter newElements: array of elements
     public mutating func append(contentsOf newElements: [T]) {
         guard newElements.count > 0 else { return }
         let end = elements.count
@@ -26,17 +38,28 @@ public struct ObservableArray<T> {
         eventsSubject.onNext(.inserted(indices: Array(end..<elements.count), elements: newElements))
     }
 
+    /// Prepend element to ObservableArray
+    ///
+    /// - Parameter element: element
     public mutating func prepend(_ element: T) {
         let index = elements.startIndex
         elements.insert(element, at: index)
         eventsSubject.onNext(.inserted(indices: [index], elements: [element]))
     }
 
+    /// Insert element to ObservableArray at given index
+    ///
+    /// - Parameters:
+    ///   - element: element
+    ///   - index: index
     public mutating func insert(_ element: T, at index: Int) {
         elements.insert(element, at: index)
         eventsSubject.onNext(.inserted(indices: [index], elements: [element]))
     }
 
+    /// Remove first element from ObservableArray
+    ///
+    /// - Returns: removed element
     public mutating func removeFirst() -> T {
         let index = elements.startIndex
         let element = elements.removeFirst()
@@ -46,6 +69,9 @@ public struct ObservableArray<T> {
         return element
     }
 
+    /// Remove last element from ObservableArray
+    ///
+    /// - Returns: removed element
     public mutating func removeLast() -> T {
         let index = elements.endIndex - 1
         let element = elements.removeLast()
@@ -55,6 +81,10 @@ public struct ObservableArray<T> {
         return element
     }
 
+    /// Remove element at index from ObservableArray
+    ///
+    /// - Parameter index: index
+    /// - Returns: removed element
     public mutating func remove(at index: Int) -> T {
         let element = elements.remove(at: index)
         defer {
@@ -63,6 +93,7 @@ public struct ObservableArray<T> {
         return element
     }
 
+    /// Remove all elements from ObservableArray
     public mutating func removeAll() {
         guard !self.elements.isEmpty else { return }
         let elements = self.elements
@@ -70,6 +101,9 @@ public struct ObservableArray<T> {
         eventsSubject.onNext(.deleted(indices: Array(elements.startIndex..<elements.endIndex), elements: elements))
     }
 
+    /// Subscript element (get and set)
+    ///
+    /// - Parameter position: index
     public subscript(position: Int) -> T {
         get {
             return elements[position]
@@ -87,6 +121,7 @@ public struct ObservableArray<T> {
 
     private let eventsSubject = PublishSubject<ObservableArrayChangeEvent<T>>()
 
+    /// Observable events emitted when content of ObservableArray changes
     public var events: Observable<ObservableArrayChangeEvent<T>> {
         return eventsSubject
     }
--- DRRxObservableArray/ObservableArrayChangeEvent.swift
+++ DRRxObservableArray/ObservableArrayChangeEvent.swift
@@ -1,9 +1,21 @@
+/// Event emitted by ObservableArray when its content changes
+///
+/// - inserted: emitted when elements are inserted
+/// - deleted: emitted when elements are removed
+/// - updated: emitted when elements are updated
 public enum ObservableArrayChangeEvent<T> {
     case inserted(indices: [Int], elements: [T])
     case deleted(indices: [Int], elements: [T])
     case updated(indices: [Int], oldElements: [T], newElements: [T])
 }
 
+/// ObservableArrayChangeEvent cases are equatable when generic type of element 
+/// inside ObservableArray which produces them is equatable
+///
+/// - Parameters:
+///   - lhs: left case
+///   - rhs: right case
+/// - Returns: true if cases are equal
 public func ==<T: Equatable>(lhs: ObservableArrayChangeEvent<T>, rhs: ObservableArrayChangeEvent<T>) -> Bool {
     switch (lhs, rhs) {
     case (.inserted(let indicesLeft, let elementsLeft),
@@ -24,6 +36,13 @@ public func ==<T: Equatable>(lhs: ObservableArrayChangeEvent<T>, rhs: Observable
     }
 }
 
+/// Arrays of ObserbaleArrayChangeEvent cases are equatable when generic type of
+/// element inside ObservableArray which produces them is equatable
+///
+/// - Parameters:
+///   - lhs: left array
+///   - rhs: right array
+/// - Returns: true if arrays are equal
 public func ==<T: Equatable>(lhs: [ObservableArrayChangeEvent<T>], rhs: [ObservableArrayChangeEvent<T>]) -> Bool {
     guard lhs.count == rhs.count else { return false }
     for index in lhs.startIndex..<lhs.endIndex {
